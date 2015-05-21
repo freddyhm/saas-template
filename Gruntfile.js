fe4e22eb3b233df1fadd472e447596823d7ea8bc
Freddy Hidalgo-Monchez <me@freddyhm.com>
2015-05-21 11:31:49 -0700

    Add sass and refactoring directory structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,17 +14,23 @@ module.exports = function(grunt){
 		},
 		copy: {
 			dist: {
-				src: ['js/vendor/**/*.js', 'img/**/*'],
+				src: ['js/vendor/**/*.js', 'img/**/*', 'css/style.css.map'],
 				dest: 'dist',
 				expand: true,
 				cwd: 'src'
 			}
 		},
+		sass: {
+			dist: {
+				files: {
+					'src/css/styles.css' : 'src/sass/all.scss'
+				}
+			}
+		},
 		concat: {
 			css: {
-		    	src: ['src/css/base/normalize.css', 'src/css/base/main.css','src/css/base/layout.css', 'src/css/base/grid.css', 
-		    		  'src/css/nav/*.css', 'src/css/header/*.css', 'src/css/module/module.css', 'src/css/module/**/*.css', 'src/css/footer/*.css'],
-		    	dest: 'dist/css/concat.css'
+		    	src: ['src/css/base/normalize.css', 'src/css/base/main.css', 'src/css/base/grid.css', 'src/css/base/layout.css', 'src/css/styles.css'],
+		    	dest: 'dist/css/styles.css'
 		  	},
 		  	js: {
 		    	src: ['src/js/plugins.js', 'src/js/main.js'],
@@ -36,12 +42,13 @@ module.exports = function(grunt){
 		    	src: 'index.html',
 		    	dest: 'dist/'
 		  	}
-		}
+		},
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-include-replace');
-	grunt.registerTask('default', ['copy', 'concat', 'includereplace', 'watch']);
+	grunt.registerTask('default', ['sass', 'copy', 'concat', 'includereplace', 'watch']);
 }
