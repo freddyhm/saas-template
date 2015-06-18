7d5896153508c303493d0b53a445381af617e558
Freddy Hidalgo-Monchez <me@freddyhm.com>
2015-06-18 12:40:04 -0700

    Add livereload, switch sass compiler from ruby to experimental c++, merge layout stylesheet in sass styles.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,39 @@
-module.exports = function(grunt){
-	
+module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
 		watch: {
-			src: {
-				files: ['src/**/*', 'index.html'],
-				tasks: ['sass', 'includereplace', 'concat', 'copy'],
+			sass: {
+				files: ['src/sass/**/*.scss'],
+				tasks: ['sass:dist']
+			},
+			livereload: {
 				options: {
-					spawn: false
+					livereload: true,
+					spawn: false	
+				},
+				files: ['dist/css/*.css']
+			}
+		},
+		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: 'compressed'
+			},
+			dist: {
+				files: {
+					'dist/css/styles.css': 'src/sass/all.scss'
 				}
 			}
 		},
 		copy: {
 			dist: {
-				src: ['js/vendor/**/*.js', 'img/**/*', 'css/style.css.map'],
+				src: ['js/vendor/**/*.js', 'img/**/*', 'css/fonts/*'],
 				dest: 'dist',
 				expand: true,
 				cwd: 'src'
 			}
 		},
-		sass: {
-			dist: {
-				files: {
-					'src/css/styles.css' : 'src/sass/all.scss'
-				}
-			}
-		},
 		concat: {
-			css: {
-		    	src: ['src/css/base/normalize.css', 'src/css/base/main.css', 'src/css/base/grid.css', 'src/css/base/layout.css', 'src/css/styles.css'],
-		    	dest: 'dist/css/styles.css'
-		  	},
 		  	js: {
 		    	src: ['src/js/plugins.js', 'src/js/main.js'],
 		    	dest: 'dist/js/concat.js'
@@ -45,10 +47,12 @@ module.exports = function(grunt){
 		},
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	/*
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-include-replace');
-	grunt.registerTask('default', ['sass', 'copy', 'concat', 'includereplace', 'watch']);
-}
+	*/
+	grunt.registerTask('default', ['watch']);
+};
